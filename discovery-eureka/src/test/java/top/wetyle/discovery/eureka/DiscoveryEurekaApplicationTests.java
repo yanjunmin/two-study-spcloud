ddbce1e652b4b7615f5f88f71024a903f1990118
company <1519484516@qq.com>
2019-10-16 17:50:02 +0800

    update

---
+++ discovery-eureka/src/test/java/top/wetyle/discovery/eureka/DiscoveryEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package top.wetyle.discovery.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiscoveryEurekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
--- simple-consumer-movie/src/main/java/top/westyle/simple/consumer/movie/SimpleConsumerMovieApplication.java
+++ simple-consumer-movie/src/main/java/top/westyle/simple/consumer/movie/SimpleConsumerMovieApplication.java
@@ -2,9 +2,10 @@ package top.westyle.simple.consumer.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SimpleConsumerMovieApplication {
 

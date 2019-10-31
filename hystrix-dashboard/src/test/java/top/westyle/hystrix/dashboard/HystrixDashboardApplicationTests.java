a19e8afa6b0aea3460c3cc1067ac76773a4a96f7
company <1519484516@qq.com>
2019-10-31 14:26:56 +0800

    update

---
+++ hystrix-dashboard/src/test/java/top/westyle/hystrix/dashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,15 @@
+package top.westyle.hystrix.dashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ hystrix-turbine/src/test/java/top/westyle/hystrix/turbine/HystrixTurbineApplicationTests.java
@@ -0,0 +1,15 @@
+package top.westyle.hystrix.turbine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+class HystrixTurbineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

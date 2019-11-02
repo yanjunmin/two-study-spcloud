e9c3a1f9ae226300792af58fb4f8cabd4b6eef34
yanjunmin <1519484516@qq.com>
2019-11-02 22:30:47 +0800

    update

---
+++ geteway-zuul/src/main/java/top/westyle/geteway/zuul/GetewayZuulApplication.java
@@ -0,0 +1,13 @@
+package top.westyle.geteway.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetewayZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetewayZuulApplication.class, args);
+    }
+
+}
---
+++ geteway-zuul/src/test/java/top/westyle/geteway/zuul/GetewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package top.westyle.geteway.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetewayZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

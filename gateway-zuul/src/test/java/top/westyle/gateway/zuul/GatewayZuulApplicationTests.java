4db43ec0e8e6d548cfdcfb650ebee4c8760aba6d
yanjunmin <1519484516@qq.com>
2019-11-07 07:50:16 +0800

    update

---
+++ gateway-zuul/src/test/java/top/westyle/gateway/zuul/GatewayZuulApplicationTests.java
@@ -0,0 +1,16 @@
+package top.westyle.gateway.zuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class GatewayZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

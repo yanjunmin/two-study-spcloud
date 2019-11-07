6b0c43cdd06573b709565c5ce2d0692362720a9e
yanjunmin <1519484516@qq.com>
2019-11-08 07:58:19 +0800

    聚合网关请求

--- gateway-zuul/src/main/java/top/westyle/gateway/zuul/GatewayZuulApplication.java
+++ gateway-zuul/src/main/java/top/westyle/gateway/zuul/GatewayZuulApplication.java
@@ -2,7 +2,10 @@ package top.westyle.gateway.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -12,4 +15,9 @@ public class GatewayZuulApplication {
         SpringApplication.run(GatewayZuulApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
---
+++ gateway-zuul/src/main/java/top/westyle/gateway/zuul/service/AggregationService.java
@@ -0,0 +1,42 @@
+package top.westyle.gateway.zuul.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import rx.Observable;
+import top.westyle.gateway.zuul.entity.User;
+
+
+@Service
+public class AggregationService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "fallback")
+    public Observable<User> getMovieUserByUserId(Long id){
+        //创建一个被观察者
+        return Observable.create(observable -> {
+            //请求电影微服务
+            User movieUser = restTemplate.getForObject("http://consumer-movie/user/{id}", User.class, id);
+            observable.onNext(movieUser);
+            observable.onCompleted();
+        });
+    }
+    public  Observable<User> getUserById(Long id) {
+        //创建被观察者
+        return Observable.create(observable -> {
+            //请求用户微服务
+            User user = restTemplate.getForObject("http://provider-user/{id}", User.class, id);
+            observable.onNext(user);
+            observable.onCompleted();
+        });
+    }
+
+    public User fallback(Long id) {
+        User user = new User();
+        user.setId(-1L);
+        return user;
+    }
+}
